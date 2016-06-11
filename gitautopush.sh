458fae8b2d75eb94b66cd9e6b3e6e02141740cd6
Rank <sojitradhawal@gmail.com>
2016-06-11 12:43:22 -0400

    Sat, Jun 11, 2016 12:43:22 PM

--- gitautopush.sh
+++ gitautopush.sh
@@ -7,7 +7,7 @@ git commit -m "$now"
 if ["`git remote -v`"]; then
 	git push -f origin master
 else
-	git remote add origin https:github.com/DhawalRank/LibApp.git
+	git remote add origin https://github.com/DhawalRank/LibApp.git
 	git push -f origin master
 fi
 
