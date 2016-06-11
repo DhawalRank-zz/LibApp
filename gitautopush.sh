cbbe1a342be62a6430580719802abd2ee47a37a2
Rank <sojitradhawal@gmail.com>
2016-06-11 12:48:12 -0400

    Sat, Jun 11, 2016 12:48:12 PM

--- gitautopush.sh
+++ gitautopush.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#/bin/bash
 
 now=`date`
 git init
 git add .
 git commit -m "$now"
-if ["`git remote -v`"]; then
+if [ "`git remote -v`" ]; then
 	git push -f origin master
 else
 	git remote add origin https://github.com/DhawalRank/LibApp.git
